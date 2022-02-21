14e5448cb41dc4843da22738a3635f7c3c0de2f3
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-02-21 09:07:19 +0300

    add test2 and 3 on getting cipher key and encoded text

--- src/main/java/Decoding1.java
+++ src/main/java/Decoding1.java
@@ -1,7 +1,30 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Decoding1 {
-    public Decoding1() {
+    private String encodedText;
+    private int cKey;
+    private final Character [] alphabets = {'A','B','C','D','E','F','G'
+            ,'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X',
+            'Y','Z'};
+    private final List<Character> listItems = Arrays.asList();
 
+
+    public Decoding1(String encodedText, int cKey) {
+        this.encodedText = encodedText;
+        this.cKey = cKey;
+    }
+    public String getEncodedText() {
+        return this.encodedText;
+    }
+    public int getcKey(){
+        return this.cKey;
+    }
+    public void setEncodedText(String encodedText){
+        this.encodedText = encodedText;
+    }
+    public void setcKey(int cKey) {
+        this.cKey = cKey;
     }
 }
--- src/test/java/Decoding1Test.java
+++ src/test/java/Decoding1Test.java
@@ -9,4 +9,18 @@ class Decoding1Test {
         Decoding1 decode = new Decoding1("C",2);
         assertEquals(true, decode instanceof Decoding1);
     }
+
+    @Test
+    public void Decoding1_getsEncodedText() {
+        Decoding1 decode = new Decoding1("C",2);
+        String expected = "C";
+        assertEquals(true, decode.getEncodedText());
+    }
+
+    @Test
+    public void Decoding1_getscKey() {
+        Decoding1 decode = new Decoding1("C",2);
+        int expected = 2;
+        assertEquals(expected, decode.getcKey());
+    }
 }
