0abfd8618154540a3f5677a48519ec73032d54fa
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-02-21 03:12:42 +0300

    add test 2, which passes on the Encoding1.test, add arguments to java class

--- src/main/java/Encoding1.java
+++ src/main/java/Encoding1.java
@@ -1,7 +1,19 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Encoding1 {
-    public Encoding1() {
+    //    Should be in private ACM
+    private String pText;
+    private int cKey;
+    private final Character [] alphabets = {'A','B','C','D','E','F','G'
+            ,'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X',
+            'Y','Z'};
+    private final List<Character> listItems = Arrays.asList();
+
+    public Encoding1(String pText , int cKey) {
+        this.pText = pText;
+        this.cKey = cKey;
     }
 
 }
