7e1ba3a9fb23ed11e06699fd152f6988b3526248
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-02-21 08:53:25 +0300

    add test1 for decoding, test fail

--- src/test/java/Decoding1Test.java
+++ src/test/java/Decoding1Test.java
@@ -4,5 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Decoding1Test {
-
+    @Test
+    public void Decoding1_InstantiateClass() {
+        Decoding1 decode = new Decoding1("C",2);
+        assertEquals(true, decode instanceof Decoding1);
+    }
 }
