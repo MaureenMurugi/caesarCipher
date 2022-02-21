ffb1938dfb8e9b44204b966bca344021b6ae0b7b
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-02-21 10:56:12 +0300

    add the syntax to the front-user logic

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -12,11 +12,17 @@ public class CaesarCipher {
         String phrase = myScanner.nextLine();
         System.out.println("Please enter a cipher key(1-25)");
         int cKey = myScanner.nextInt();
+        Encoding1 encoding1 = new Encoding1(phrase,cKey);
+        if(encoding1.isValidKey()){
+            Decoding1 decoding1 = new Decoding1(encoding1.encodePText(),cKey);
+            System.out.println("Input String: "+encoding1.encodePText());
+            System.out.println("Encrypted String: "+decoding1.decodeEncodedText());
+        }else {
+            System.out.println("Error");
+        }
     }
 
-//Front-interface
-    public CaesarCipher() {
-    }
+
 
 
 
